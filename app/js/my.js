a7fc81080dc6a49b7d607ff50441bef8f72670e6
Vad <vad@sovesti.net>
2016-04-06 06:53:49 +0300

    менюшка

---
+++ app/js/my.js
@@ -0,0 +1,36 @@
+var a1 = 5; var b1 = 10;
+
+// var flag=false;
+// var arr = [1,'2222',flag];
+
+
+console.log( (a1*a1+b1*b1)/2 );
+
+console.log( ( Math.abs(a1)+Math.abs(b1) )/2 );
+
+var a1 = [5,5];
+var b1 = [7,4];
+
+res = Math.abs(a1[0]-b1[0]) + Math.abs(a1[1]-b1[1]); 
+
+
+if (res == 3) { 
+    console.log(1)
+} else { 
+    console.log(0)
+};
+
+
+
+
+
+if ('если жизнь на марсе') {
+    res = "ура! жизнь есть!"
+} else {
+    res = "облом =( жизни нет!"
+}
+
+
+// alert('hi');
+console.log(res);
+
